672e45993883c795c4a7bfa89951cdd6d35c4a61
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-02 13:06:01 +0300

    added devise and configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  email: 'anthonyshikanga@gmail.com',
+  password: 'c1b2d3t4a5'
+
+	)

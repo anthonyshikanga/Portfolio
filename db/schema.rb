0402872c1baa113830bfb556e86500580becdd88
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-17 11:08:15 +0300

    installed carrier wave gem for image uploads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803080617) do
+ActiveRecord::Schema.define(version: 20170817080014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170803080617) do
     t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|

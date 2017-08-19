b04a3ef3be842baab849ca3f8ad1efd0ba7b2b8b
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-19 23:32:31 +0300

    added field for comments in comment table

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,59 @@
+class CommentsController < ApplicationController
+	before_action :check_user, only: [:new,:create,:edit,:update,:delete]
+
+	def new
+		@comment = Comment.new
+	end
+
+	def create
+		@comment = Comment.new(comment_params)
+		if @comment.save
+			flash[:notice] = "Comment added succesfully"
+			redirect_to post_path(@post)
+		else
+			render :new
+		end
+	end
+
+	def edit 
+		@comment = Comment.find(params[:id])
+	end
+
+	def update
+		@comment =Comment.find(params[:id])
+		if @comment.user == current_user && @comment.update(comment_params)
+			redirect_to post_path(@post)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		if @comment.user == current_user && @comment.delete
+			flash[:notice] == 'Comment deleted succesfully'
+			redirect_to post_path(@post)
+		else
+			flash[:alert] = "Failed to delete comment"
+		end
+	end
+
+	private
+	def check_user
+		@post = Post.find(params[:post_id])
+		if user_signed_in?
+			@user = current_user
+		else
+			flash[:alert] = "You have to sign in to post a comment"
+			redirect_to post_path(@post)
+		end
+	end
+
+	def comment_params
+		post = {'post_id' :@post.id}
+		user = {'user_id' :@user.id}
+		params.require(:comment).permit(:comment,:post_id).merge(post).merge(user)
+		end
+	end
+
+	

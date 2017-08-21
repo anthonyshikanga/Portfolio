d50e629f708af5ae26b61eb101b9b79b886d914f
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-21 08:57:22 +0300

    scaffold reference resource model views and contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :references
   resources :posts do
   	resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819203510) do
+ActiveRecord::Schema.define(version: 20170821055419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20170819203510) do
     t.string "name"
     t.string "link"
     t.integer "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "references", force: :cascade do |t|
+    t.string "author"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

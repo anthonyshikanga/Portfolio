37227d56de8ba266ca9d82e395b5acf319ebeec7
anthonyshikanga <anthonyshikanga@gmail.com>
2017-08-23 15:58:38 +0300

    modified application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,5 @@
 //= require rails-ujs
 //= require_tree
 //= require jquery
-//=require jquery.turbolinks
 //=require jquery_ujs
 //= require bootstrap-sprockets
-//= require froala_editor.min.js 
-//= require turbolinks

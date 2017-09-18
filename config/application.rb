f370585456500c0e9c3e52f099404c4c8418773b
anthonyshikanga <anthonyshikanga@gmail.com>
2017-09-18 12:05:54 +0300

    modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.initialize_on_precompile = false
 
     
 
